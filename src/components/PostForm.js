12ffc369d6be87d882559fc6e36f9766bee7e2d8
Aram <68351683+Aram442@users.noreply.github.com>
2021-03-12 10:53:25 +0400

    first-commit

---
+++ src/components/PostForm.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { createPost } from '../redux/action'
+class PostForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            title: ''
+        }
+    }
+    submitHandler = (event) => {
+        event.preventDefault()
+        const { title } = this.state
+        if (!title.trim()) {
+            return
+        }
+        const newPost = {
+            title, id: Date.now().toString()
+        }
+        this.props.createPost(newPost)
+        this.setState({ title: '' })
+    }
+    changeInputHandler = (event) => {
+        this.setState(prev => ({
+            ...prev, ...{
+                [event.target.name]: event.target.value
+            }
+        }))
+    }
+    render() {
+        return (
+            <form onSubmit={this.submitHandler}>
+                <div className="form-group">
+                    <label htmlFor="title" >Заголовок пост</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="title"
+                        name='title'
+                        value={this.state.title}
+                        onChange={this.changeInputHandler}
+                    />
+                </div>
+                <button className='btn btn-success mt-2' type='submit'>Создать</button>
+            </form>
+        )
+    }
+}
+const mapDispatchToProps = {
+    createPost
+}
+export default connect(null, mapDispatchToProps)(PostForm)
